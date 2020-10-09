88ce2ac8409b51b9e7ff1600ea5dec1fbd715a19
Dominique Garrett <dmnqgrrtt@gmail.com>
2020-10-08 20:02:06 -0400

    moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,27 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    no_zero = True
+    for num in arr:
+        if num == 0:
+            no_zero = False
+    if no_zero == True:
+        return arr
 
-    pass
+    index = 0
+    while index < (len(arr) - 1):
+        if arr[index] == 0:
+            for i in range(index + 1, len(arr)):
+                if arr[i] != 0:
+                    arr[index], arr[i] = arr[i], arr[index]
+        index += 1
+
+    return arr
+
+
+
+
+    
 
 
 if __name__ == '__main__':
